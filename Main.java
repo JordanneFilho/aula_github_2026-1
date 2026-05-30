192cd5617873860a4747986d27c423f1724a40d3
Emmanul-SR <shirogame2019@gmail.com>
2026-05-30 16:33:07 -0300

    #2 Criar classe Cliente; Adicionar atributos básicos ( nome ,cpf,telefone)

--- Main.java
+++ Main.java
@@ -1,11 +1,29 @@
 import java.util.Arrays;
 
 public class Main {
+    public static void main(String[] args) {
+        int opcao = 0;
 
-	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
-		System.out.println("Fim");
-	}
+        do {
+            Menu mainMenu = new Menu("Menu Principal", Arrays.asList(
+                "Cliente",
+                "Saque",
+                "Sair"
+            ));
+            opcao = mainMenu.getSelection();
 
+            switch (opcao) {
+                case 1:
+                    CadastrarCliente.menu();
+                    break;
+                case 2:
+                    RealizarSaque.menu();
+                    break;
+                case 3:
+                    System.out.println("Encerrando sistema...");
+                    break;
+            }
+
+        } while (opcao != 3);
+    }
 }
